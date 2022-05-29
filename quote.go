2b778dcc79938219fe495ca08855b47256f4e54a
Pascal Heinrich <pascalheinrich.de@googlemail.com>
2022-05-29 18:13:23 +0200

    fix key leading zero

--- quote.go
+++ quote.go
@@ -2,12 +2,12 @@ package av
 
 type GlobalQuote struct {
 	Quote struct {
-		Symbol          string  `json:"1. symbol"`
-		Open            float64 `json:"2. open,string"`
-		High            float64 `json:"3. high,string"`
-		Low             float64 `json:"4. low,string"`
-		Price           float64 `json:"5. price,string"`
-		Volume          int64   `json:"6. volume,string"`
+		Symbol          string  `json:"01. symbol"`
+		Open            float64 `json:"02. open,string"`
+		High            float64 `json:"03. high,string"`
+		Low             float64 `json:"04. low,string"`
+		Price           float64 `json:"05. price,string"`
+		Volume          int64   `json:"06. volume,string"`
 		LatesTradingDay string  `json:"07. latest trading day"`
 		PreviousClose   float64 `json:"08. previous close,string"`
 		Change          float64 `json:"09. change,string"`
